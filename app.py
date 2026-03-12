01143c8cdd55e6a0633e1494a98a50e25a4d76aa
maksim-ivt22 <likewalker@mail.ru>
2026-03-12 18:00:08 +0900

    lab5

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import json
 from flask import Flask, abort, jsonify, request
-from flask_restx import Api  # type: ignore
+from flask_restx import Api, Resource  # type: ignore
 
 PAGE_SIZE = 25
 
@@ -10,7 +10,8 @@ api = Api(app)
 with open('awards.json', encoding='utf-8') as f:
     awards = json.load(f)
 
-# TODO: Добавить код для чтения лауреатов из файла
+with open('laureats.json', encoding='utf-8') as f:
+    laureats = json.load(f)
 
 
 @app.route("/api/v1/awards/")
@@ -21,7 +22,9 @@ def awards_list():
             raise ValueError
     except ValueError:
         return abort(400)
-    page = awards[p * 50:(p + 1)*50]
+
+    page = awards[p * 50:(p + 1) * 50]
+
     return jsonify({
         'page': p,
         'count_on_page': PAGE_SIZE,
@@ -38,7 +41,22 @@ def award_object(pk):
         abort(404)
 
 
-# TODO: Добавить код для получения списка лауреатов
+@api.route("/v2/laureats/")
+class LaureatsList(Resource):
+    def get(self):
+        return jsonify({
+            "total": len(laureats),
+            "items": laureats
+        })
 
 
-# TODO: Добавить код для получения лауреата по индексу
+@api.route("/v2/laureat/<int:pk>/")
+class Laureat(Resource):
+    def get(self, pk):
+        if 0 <= pk < len(laureats):
+            return jsonify(laureats[pk])
+        else:
+            abort(404)
+
+if __name__ == "__main__":
+    app.run(debug=True)
